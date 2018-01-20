df9406307947e81ef4c0d6fc404d4bcf1bee1c71
roeylrz <35033332+roeylrz@users.noreply.github.com>
2018-01-20 21:48:27 +0200

    add patch

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 //We want the server only to handle our routs
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -60,6 +61,34 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+//PATCH - wht we use when we want to update a resource. there are other options as well
+app.patch('/todos/:id', (req, res) => {
+    //Using pick we can take only properties that we want to take from the client and 
+    //not thing that we dont want to let the clients change    
+    var body =_.pick(req.body, ['text', 'completed']); //here we take from the client only text and completed ad only if there exists
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }  
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    } else {
+        body.comleted = false;
+        body.completedAt = null //If we want to remove a value from the database, we set it to null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {//{$set: body} - $set takes a set of key value pairs and set them, {new: true} - Mongoose version to bring back the updated values from the database and not the original
+    if (!todo) {
+        return res.status(404).send();
+    }
+    res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
